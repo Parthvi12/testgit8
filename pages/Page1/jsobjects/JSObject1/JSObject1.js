276c071defdd19f9d0f0f47c3add5f7e488a8262
parthvi12 <parthvi12@appsmith.com>
2023-02-03 09:37:40 +0000

    commit

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -1,8 +1,8 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	myFun1: () => {
-			return "testing";
+	myFun18: () => {
+			return "hi";
 	},
 	myFun2: async () => {
 		//use async-await or promises
